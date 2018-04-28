4e29f412c1d897e9e6fbcb6b7a969a773c9e0495
Rustem Akhmadeev <dxza@ya.ru>
2018-04-28 22:52:01 +0300

    bugFix

--- index.php
+++ index.php
@@ -27,16 +27,14 @@ $user_avatar = 'img/user.jpg';
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
-            <?php if ($is_auth) { ?>
+            <?php if ($is_auth): ?>
                 <div class="user-menu__image">
                     <img src="<?= $user_avatar ?>" width="40" height="40" alt="Пользователь">
                 </div>
                 <div class="user-menu__logged">
                     <p><?= $user_name ?></p>
                 </div>
-            <?php } else { ?>
+            <?php else: ?>
                 <ul class="user-menu__list">
                     <li class="user-menu__item">
                         <a href="#">Регистрация</a>
@@ -45,7 +43,7 @@ $user_avatar = 'img/user.jpg';
                         <a href="#">Вход</a>
                     </li>
                 </ul>
-            <?php } ?>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
